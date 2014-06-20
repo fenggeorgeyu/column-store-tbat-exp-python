435fe30b871e81bd6f81269f975d9d57fc796903
morgenfree <fyu@ysu.edu>
2014-06-20 17:38:10 -0400

    write file using buffer
    __init__.py to initialize package variables
    
    prepareData
         BAT, TBAT, update
         per is the percentage of update
    
    update experiment in a roll in exp_update/exp_updateData.py
    
    problem:
          the overhead is decreasing...

--- exp_update/config.py
+++ exp_update/config.py
@@ -5,12 +5,12 @@ from prepare import data_dir,suffix
 
 num_lines_1m=47660
 num_lines_1g=num_lines_1m*1024
-num_lines=num_lines_1m*10
+num_lines=num_lines_1m*1
 
 bat_file_name=data_dir+'bat'+suffix # BAT
 tbat_file_name=data_dir+'tbat'+suffix # TBAT
 
-pers=range(0.1,0.1,1) # update percentage
+pers=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1] # update percentage
 
 
 result_file_name=data_dir+'result'+time.strftime("%y%m%d-%Ih%Mm%Ss")+'.txt'
