e284d649fc597b760add9e752afb1942bcd11456
Feng George Yu <fyu@ysu.edu>
2014-09-26 01:23:53 -0400

    change in exp_update/config.py:
     experiment time=10
     percentage=0.1 to 0.5

--- exp_update/config.py
+++ exp_update/config.py
@@ -3,7 +3,7 @@ __author__ = 'fyu'
 import os, time, datetime
 #from prepare import data_dir,suffix
 
-max_exp_times=1
+max_exp_times=10
 
 # ---------- number of lines in the testing data file---------
 num_lines_1m=47660
@@ -14,8 +14,9 @@ num_lines_1g=num_lines_1m*1024
 num_lines=num_lines_1m
 
 #-------------update percentage---------------
-pers=[0.1]
-#pers=[0.1,0.2,0.3,0.4,0.5]
+#pers=[0.1]
+#pers=[0.1, 0.2, 0.3]
+pers=[0.1,0.2,0.3,0.4,0.5]
 #pers=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
 
 
