87207fddc9d38faba96b21e9a8b76489e16d40ba
morgenfree <fyu@ysu.edu>
2014-09-27 10:25:33 -0400

    revised batch run exp update script

--- script/run_exp_update_slow_multiple_batch.sh
+++ script/run_exp_update_slow_multiple_batch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-now=$(date +%Y%m%d-%Hh%Mm%Ss)
-
 if [ -z "$1" ]
   then
     echo "Please input your email password!"
@@ -12,20 +10,23 @@ num_lines_1m=47660
 num_lines_1g=$(( num_lines_1m * 1024 ))
 num_lines=${num_lines_1m}
 #num_lines=100
-max_exp_times=20
+max_exp_times=10
 max_run_times=3
 pers="0.1 0.2 0.3 0.4 0.5"
 
 cd ..
 
-for i in {1 .. ${max_run_times}}
+for (( i=1; i<=max_run_times; i++ ))
 do
+    now=$(date +%Y%m%d-%Hh%Mm%Ss)
     python exp_update/exp_updateData3.py ${num_lines} ${max_exp_times} ${pers}
+
+    cp data/result.txt data/result-l${num_lines}-expt-${max_exp_times}${now}.txt
 
     #---send email when finished
     echo -e "sending email to me"
     tmp=temp_email
-    title="TBAT Exp "${now}
+    title="TBAT Exp on "`hostname`" at "${now}
     touch ${tmp}
     #add category for blog
     echo "">>${tmp}
@@ -47,4 +48,5 @@ do
     sendEmail -f ${sendfrom} -t ${sendto} -s ${smtp_server} -o tls=yes -xu ${xuser} -xp ${xp} -u ${title} < ${tmp}
 
     rm ${tmp}
+    echo ${i}
 done
